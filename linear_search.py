4145baefa616c45832c8286aa6af6caecb1926d6
mohan putta <puttamohan2001@gmail.com>
2022-12-17 12:51:28 +0530

    search algorithem practice

---
+++ linear_search.py
@@ -0,0 +1,21 @@
+import time
+
+num = range(1,500+1)
+
+target = 500
+Tries = 1
+value = -1
+
+initial_time = time.time()
+for item in num:
+    if item == target:
+        value = item
+        break
+    Tries += 1
+    time.sleep(.1)
+
+final_time = time.time()
+#if it return nagitive value then the existence of target is absent.
+print("target found: {}".format(value))
+print("No of Tries: {}".format(Tries))
+print("Excution time: {}".format(final_time-initial_time))
